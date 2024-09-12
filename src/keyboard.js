6351a6cbe1dddccbbb4c11fc707b5399e20ad4a7
Shea <96454908+AlabasterLeech@users.noreply.github.com>
2024-09-12 12:10:57 -0400

    Key functionality and vowel layout
    Added layout for vowels using a table that roughly mimics the IPA vowel chart (sometimes very roughly, but I think I've achieved a good balance between accuracy and complexity).
    
    Additionally, worked out the basic typing functionality and added it to a single button. Next to do is make the other buttons, style them up nice, and make sure the code adding the event listener to all buttons is adding it to all the buttons.

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,7 +1,23 @@
+let linkedTextArea = document.getElementById("keyboard-linked"); //Text area linked to keyboard
+let allKeys = document.getElementsByClassName("key");
+
+//Add event listeners for all keyboard keys
+for(let key of allKeys){
+    key.addEventListener("click", keyType)
+}
+
+//function called by keyboard keys to type in linked text area
+function keyType(evt){
+    let content = linkedTextArea.value;
+    content += evt.currentTarget.innerText;
+    linkedTextArea.value = content;
+    window.alert("Typed")
+}
+
+//function used by buttons controlling tabbed interface for keyboard
 function openTab(evt, tabName) {
     let tabcontent = document.getElementsByClassName("tabcontent");
     let tablinks = document.getElementsByClassName("tablinks");
-
     //Hide all tabs
     for (let i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
