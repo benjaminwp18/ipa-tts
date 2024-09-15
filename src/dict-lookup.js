10edc321fc7b2b2fd4c2787a0a8dc44b210aa545
Kasey Wei <kasey.wei121@gmail.com>
2024-09-15 11:08:26 -0400

    add error handling, return with status

--- src/dict-lookup.js
+++ src/dict-lookup.js
@@ -1,6 +1,6 @@
 // \p{L} matches any letter from any language
 // \p{M} matches character intended to be combined with another char (e.g. accents)
-const NON_ALPHANUM_REGEX = /[^\p{L}|\p{M}|\d]+/ugm;
+const NON_ALPHANUM_REGEX = /[^\p{L}|\p{M}]+/ugm; // TODO: include hyphen? apostrophe?
 
 let currentLang;
 let currentDict;
@@ -22,6 +22,9 @@ function parseDict(text) {
 
 async function loadDict(lang) {
     const response = await fetch(generateDictUrl(lang));
+    if (!response.ok) {
+        throw new Error(`Status ${response.status}`);
+    }
     const text = await response.text();
     currentDict = parseDict(text);
 }
@@ -35,31 +38,45 @@ function findEntries(phrase) {
         return [entry];
 
     // if that doesn't work, try splitting phrase
-    const words = formattedPhrase.split(NON_ALPHANUM_REGEX);
-    if (words.length > 1) {
-        let entries = [];
-        words.forEach(word => {
-            const entry = currentDict.find(e => e.word === word);
-            entry ? entries.push(entry) : entries.push({ word: word, ipa: null });
-        });
-        return entries;
-    }
-
-    return [{ word: phrase, ipa: null }];
+    const words = formattedPhrase
+        .split(NON_ALPHANUM_REGEX)
+        .filter(Boolean); // remove empty strings
+    let entries = [];
+    words.forEach(word => {
+        const entry = currentDict.find(e => e.word === word);
+        entry ? entries.push(entry) : entries.push({ word: word, ipa: null });
+    });
+    return entries;
 }
 
 async function translateIpa(phrase, lang) {
     if (!currentDict || currentLang !== lang) {
-        await loadDict(lang);
-        currentLang = lang;
+        try {
+            await loadDict(lang);
+            currentLang = lang;
+        } catch (error) {
+            return {
+                status: 'HTTP_ERROR',
+                details: `Could not load dictionary for ${lang}: ${error.message}`
+            };
+        }
     }
 
-    const entries = await findEntries(phrase, lang);
-    const wordsNotFound = entries.filter(entry => entry.ipa === null).map(entry => entry.word);
-    if (wordsNotFound.length > 0) {
-        return `The following word(s) were not found: ${wordsNotFound.join(', ')}`;
+    const entries = findEntries(phrase, lang);
+    if (entries.length === 0) {
+        return { status: 'NO_VALID_WORD' };
     }
-    else {
-        return entries.map(entry => entry.ipa).join(' ');
+
+    const wordsNotFound = entries.filter(entry => !entry.ipa).map(entry => entry.word);
+    if (wordsNotFound.length > 0) {
+        return {
+            status: 'NOT_FOUND',
+            words: wordsNotFound
+        };
+    } else {
+        return {
+            status: 'SUCCESS',
+            words: entries.map(entry => entry.ipa)
+        }
     }
 }
