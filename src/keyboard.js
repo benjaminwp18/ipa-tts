7bba4d2db4fc10a67ae8caef8caf81ae4c468ac8
Wendy Wu <96844488+ww-classy-lady@users.noreply.github.com>
2024-09-17 12:05:18 -0400

    Squashed commit of the following:
    commit 4285be4670b0b6d0749e6c4e14373d20a86952e7
    Merge: 32ba1d2 d2bce67
    Author: KaseyW31 <115895250+KaseyW31@users.noreply.github.com>
    Date:   Tue Sep 17 11:57:17 2024 -0400
    
        Merge pull request #3 from benjaminwp18/ipa-dict
    
        Ipa dict
    
    commit 32ba1d2877e3e21f6b1d526fcf0e89ade6466be6
    Merge: 9f5cc26 5cee91b
    Author: Shea <96454908+AlabasterLeech@users.noreply.github.com>
    Date:   Tue Sep 17 11:52:41 2024 -0400
    
        Merge pull request #4 from benjaminwp18/shea-keyboard
    
        Vowel Keyboard Implementation
    
    commit 5cee91b7f0d563a1e9cdade1ef4eca249a279e4e
    Author: benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
    Date:   Tue Sep 17 11:52:13 2024 -0400
    
        The children must be tabbed
    
    commit 54d09f22113204f0025228c4fc940097e039ba9e
    Author: benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
    Date:   Tue Sep 17 11:30:41 2024 -0400
    
        Regularize tabbing
    
    commit d323bff2cd572cfca7959e0f00948a98300bbbf2
    Author: benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
    Date:   Tue Sep 17 11:20:14 2024 -0400
    
        Import CSS, JS style, use class for tab display
    
    commit 2365e13d69e7bf0a727109b656da3a1e99b7d2f3
    Merge: ca122f1 9f5cc26
    Author: Shea <96454908+AlabasterLeech@users.noreply.github.com>
    Date:   Mon Sep 16 12:49:06 2024 -0400
    
        Merge branch 'main' into shea-keyboard
    
    commit ca122f1d9f8b3e175946a6f6528541cd1b29c94c
    Author: Shea <96454908+AlabasterLeech@users.noreply.github.com>
    Date:   Mon Sep 16 11:39:01 2024 -0400
    
        Vowel keyboard complete incl. diacritics
    
    commit d2bce67ecade309bfa6fa56fa6d1f5f3464c724f
    Author: Kasey Wei <kasey.wei121@gmail.com>
    Date:   Sun Sep 15 11:48:37 2024 -0400
    
        export translateIpa, add docstring
    
    commit 10edc321fc7b2b2fd4c2787a0a8dc44b210aa545
    Author: Kasey Wei <kasey.wei121@gmail.com>
    Date:   Sun Sep 15 11:08:26 2024 -0400
    
        add error handling, return with status
    
    commit 2fbd6813c9387ab8dc4e1b019f0a236ec2edd415
    Author: Kasey Wei <kasey.wei121@gmail.com>
    Date:   Sat Sep 14 23:27:05 2024 -0400
    
        support multiple words, add trim + lowercase
    
    commit e792ea7205eca4d245ae9ab19935e7b7a618105d
    Author: Kasey Wei <kasey.wei121@gmail.com>
    Date:   Thu Sep 12 23:00:39 2024 -0400
    
        add functions to search dicts
    
    commit 6351a6cbe1dddccbbb4c11fc707b5399e20ad4a7
    Author: Shea <96454908+AlabasterLeech@users.noreply.github.com>
    Date:   Thu Sep 12 12:10:57 2024 -0400
    
        Key functionality and vowel layout
    
        Added layout for vowels using a table that roughly mimics the IPA vowel chart (sometimes very roughly, but I think I've achieved a good balance between accuracy and complexity).
    
        Additionally, worked out the basic typing functionality and added it to a single button. Next to do is make the other buttons, style them up nice, and make sure the code adding the event listener to all buttons is adding it to all the buttons.
    
    commit c7288057d69f290b77ecc8f3e2bf6549d57bc7af
    Author: Shea <96454908+AlabasterLeech@users.noreply.github.com>
    Date:   Tue Sep 10 12:41:14 2024 -0400
    
        Added tabbed interface to hold keyboard

---
+++ src/keyboard.js
@@ -0,0 +1,31 @@
+const linkedTextArea = document.getElementById("keyboard-linked"); // Text area linked to keyboard
+const allKeys = document.getElementsByClassName("key");
+const tabcontent = document.getElementsByClassName("tabcontent");
+const tablinks = document.getElementsByClassName("tablinks");
+
+// Add event listeners for all keyboard keys
+for (let key of allKeys) {
+    key.addEventListener("click", keyType)
+}
+
+// function called by keyboard keys to type in linked text area
+function keyType(evt) {
+    let content = linkedTextArea.value;
+    content += evt.currentTarget.innerText;
+    linkedTextArea.value = content;
+}
+
+// function used by buttons controlling tabbed interface for keyboard
+function openTab(evt, tabName) {
+    // Hide all tabs
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("open");
+    }
+    // Remove class active from all buttons
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    // Open activated tab and add class active to the button clicked
+    document.getElementById(tabName).classList.add("open");
+    evt.currentTarget.classList.add("active");
+}
