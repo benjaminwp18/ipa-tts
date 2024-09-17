d323bff2cd572cfca7959e0f00948a98300bbbf2
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2024-09-17 11:20:14 -0400

    Import CSS, JS style, use class for tab display

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,32 +1,31 @@
-let linkedTextArea = document.getElementById("keyboard-linked"); //Text area linked to keyboard
-let allKeys = document.getElementsByClassName("key");
+const linkedTextArea = document.getElementById("keyboard-linked"); // Text area linked to keyboard
+const allKeys = document.getElementsByClassName("key");
+const tabcontent = document.getElementsByClassName("tabcontent");
+const tablinks = document.getElementsByClassName("tablinks");
 
-//Add event listeners for all keyboard keys
-for(let key of allKeys){
+// Add event listeners for all keyboard keys
+for (let key of allKeys) {
     key.addEventListener("click", keyType)
 }
 
-//function called by keyboard keys to type in linked text area
-function keyType(evt){
+// function called by keyboard keys to type in linked text area
+function keyType(evt) {
     let content = linkedTextArea.value;
     content += evt.currentTarget.innerText;
     linkedTextArea.value = content;
 }
 
-//function used by buttons controlling tabbed interface for keyboard
+// function used by buttons controlling tabbed interface for keyboard
 function openTab(evt, tabName) {
-    let tabcontent = document.getElementsByClassName("tabcontent");
-    let tablinks = document.getElementsByClassName("tablinks");
-    //Hide all tabs
+    // Hide all tabs
     for (let i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+        tabcontent[i].classList.remove("open");
     }
-    //Remove class active from all buttons
-    tablinks = document.getElementsByClassName("tablinks");
+    // Remove class active from all buttons
     for (let i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].classList.remove("active");
     }
-    //Open activated tab and add class active to the button clicked
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    // Open activated tab and add class active to the button clicked
+    document.getElementById(tabName).classList.add("open");
+    evt.currentTarget.classList.add("active");
 }
